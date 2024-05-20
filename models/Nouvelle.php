576f25f6919307aa42c52cddf3dab806e1598cbe
eriga <ombreslumieres@hotmail.com>
2024-05-19 20:05:42 -0400

    first commit

---
+++ models/Nouvelle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Models;
+
+use Bases\Model;
+
+class Nouvelle extends Model {
+    // La propriété $table doit être surchargée pour que le modèle précise sa table associée
+    protected $table = "nouvelles";
+}
